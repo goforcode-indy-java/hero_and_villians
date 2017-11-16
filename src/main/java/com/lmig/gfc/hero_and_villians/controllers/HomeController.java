8936b75e43ff6a4c872feffe607fba83e91d8172
Eric Schwartz <eric.schwartz@theironyard.com>
2017-11-16 16:16:31 -0500

    starting point for multi villians

--- src/main/java/com/lmig/gfc/hero_and_villians/controllers/HomeController.java
+++ src/main/java/com/lmig/gfc/hero_and_villians/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.lmig.gfc.hero_and_villians.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,7 +12,7 @@ import com.lmig.gfc.hero_and_villians.models.Monster;
 @Controller
 public class HomeController {
 	
-	private Monster ourMonster;
+	private ArrayList<Monster> ourMonsters;
 	private Hero ourHero;
 	
 	public HomeController() {
@@ -20,47 +22,34 @@ public class HomeController {
 	@RequestMapping("/")
 	public ModelAndView defaultPage() {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("monster", ourMonster);
+		mv.addObject("monsters", ourMonsters);
 		mv.addObject("hero", ourHero);
 		
 		mv.setViewName("index");
 		return mv;
 	}
 	
-	@RequestMapping("/attack")
-	public ModelAndView attackPage() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("monster", ourMonster);
-		mv.addObject("hero", ourHero);
-		
-		ourHero.attack(ourMonster);
-		
-		mv.addObject("isGameOver", this.isGameOver());
-		
-		mv.setViewName("index"); 
-		return mv;
-	}
 	
-	@RequestMapping("/fight")
-	public ModelAndView fightPage() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("monster", ourMonster);
-		mv.addObject("hero", ourHero);
-		
-		while(this.isGameOver() == false) {
-			ourHero.attack(ourMonster);
-		}
-		
-		//same as above but loop always runs at least 1 time
-//		do {
+//	@RequestMapping("/fight")
+//	public ModelAndView fightPage() {
+//		ModelAndView mv = new ModelAndView();
+//		mv.addObject("monsters", ourMonsters);
+//		mv.addObject("hero", ourHero);
+//		
+//		while(this.isGameOver() == false) {
 //			ourHero.attack(ourMonster);
-//		} while (this.isGameOver() == false);
-		
-		mv.addObject("isGameOver", this.isGameOver());
-		
-		mv.setViewName("index"); 
-		return mv;
-	}
+//		}
+//		
+//		//same as above but loop always runs at least 1 time
+////		do {
+////			ourHero.attack(ourMonster);
+////		} while (this.isGameOver() == false);
+//		
+//		mv.addObject("isGameOver", this.isGameOver());
+//		
+//		mv.setViewName("index"); 
+//		return mv;
+//	}
 	
 	@RequestMapping("/reset")
 	public ModelAndView resetPage() {
@@ -68,20 +57,34 @@ public class HomeController {
 		
 		this.initGame();
 		
-		mv.addObject("monster", ourMonster);
+		mv.addObject("monsters", ourMonsters);
 		mv.addObject("hero", ourHero);
 		
 		mv.setViewName("index"); 
 		return mv;
 	}
 	
-	public boolean isGameOver() {
-//		return (!ourHero.isAlive() || !ourMonster.isAlive());
-		return (ourHero.isAlive() == false || ourMonster.isAlive() == false);
-	}
+//	public boolean isGameOver() {
+////		return (!ourHero.isAlive() || !ourMonster.isAlive());
+//		return (ourHero.isAlive() == false || ourMonster.isAlive() == false);
+//	}
 	
 	public void initGame() {
-		ourMonster = new Monster("Steve", 80, "Ogre");
+		
+		ourMonsters = new ArrayList<Monster>();
+		
+		Monster monster1 = new Monster("Steve", 80, "Ogre");
+		Monster monster2 = new Monster("Bob", 75, "Vampire");
+		Monster monster3 = new Monster("Joe", 90, "Warewolf");
+		Monster monster4 = new Monster("Billy", 100, "Psycho Killer");
+		Monster monster5 = new Monster("Doug", 20, "Coding Monster");
+		
+		ourMonsters.add(monster1);
+		ourMonsters.add(monster2);
+		ourMonsters.add(monster3);
+		ourMonsters.add(monster4);
+		ourMonsters.add(monster5);
+		
 		ourHero = new Hero("Clark Kent", 100, "Superman"); 
 	}
 	
