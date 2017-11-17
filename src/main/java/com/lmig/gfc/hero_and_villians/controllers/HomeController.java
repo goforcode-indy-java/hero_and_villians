d8ea2234b7972182b76fe617b043d17105e83a5a
Eric Schwartz <eric.schwartz@theironyard.com>
2017-11-16 20:33:32 -0500

    adding simple solution hopefully we can understand

--- src/main/java/com/lmig/gfc/hero_and_villians/controllers/HomeController.java
+++ src/main/java/com/lmig/gfc/hero_and_villians/controllers/HomeController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lmig.gfc.hero_and_villians.models.Hero;
+import com.lmig.gfc.hero_and_villians.models.LivingThing;
 import com.lmig.gfc.hero_and_villians.models.Monster;
 
 @Controller
 public class HomeController {
 	
-	private ArrayList<Monster> ourMonsters;
+	private ArrayList<LivingThing> ourVillains;
 	private Hero ourHero;
 	
 	public HomeController() {
@@ -22,7 +23,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public ModelAndView defaultPage() {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("monsters", ourMonsters);
+		mv.addObject("villains", ourVillains);
 		mv.addObject("hero", ourHero);
 		
 		mv.setViewName("index");
@@ -30,26 +31,30 @@ public class HomeController {
 	}
 	
 	
-//	@RequestMapping("/fight")
-//	public ModelAndView fightPage() {
-//		ModelAndView mv = new ModelAndView();
-//		mv.addObject("monsters", ourMonsters);
-//		mv.addObject("hero", ourHero);
-//		
-//		while(this.isGameOver() == false) {
-//			ourHero.attack(ourMonster);
-//		}
-//		
-//		//same as above but loop always runs at least 1 time
-////		do {
-////			ourHero.attack(ourMonster);
-////		} while (this.isGameOver() == false);
-//		
-//		mv.addObject("isGameOver", this.isGameOver());
-//		
-//		mv.setViewName("index"); 
-//		return mv;
-//	}
+	@RequestMapping("/fight")
+	public ModelAndView fightPage() {
+		ModelAndView mv = new ModelAndView();
+		
+		for(LivingThing livingThing : ourVillains) {
+			
+			while(ourHero.isAlive() && livingThing.isAlive()) {
+				ourHero.attack(livingThing);
+			}
+			
+			//if the game is over lets just stop
+			if(this.isGameOver()) {
+				break;
+			}
+			
+		}
+		
+		mv.addObject("villains", ourVillains);
+		mv.addObject("hero", ourHero);
+		mv.addObject("isGameOver", this.isGameOver());
+		
+		mv.setViewName("index"); 
+		return mv;
+	}
 	
 	@RequestMapping("/reset")
 	public ModelAndView resetPage() {
@@ -57,35 +62,54 @@ public class HomeController {
 		
 		this.initGame();
 		
-		mv.addObject("monsters", ourMonsters);
+		mv.addObject("villains", ourVillains);
 		mv.addObject("hero", ourHero);
 		
 		mv.setViewName("index"); 
 		return mv;
 	}
 	
-//	public boolean isGameOver() {
-////		return (!ourHero.isAlive() || !ourMonster.isAlive());
-//		return (ourHero.isAlive() == false || ourMonster.isAlive() == false);
-//	}
+	public boolean isGameOver() {
+		
+		if(!ourHero.isAlive()) {
+			return true;
+		}
+		
+		
+		//our hero has to be alive if we made it here
+		//lets determine if any monsters (or generic living thing) are alive
+		
+		//assume all dead
+		boolean isOver = true;
+		for(LivingThing livingThing : ourVillains) {
+			if(livingThing.isAlive()) {
+				
+				//if we find one alive, set is over to false
+				isOver = false;
+				break;
+			}
+		}
+		
+		return isOver;
+	}
 	
 	public void initGame() {
 		
-		ourMonsters = new ArrayList<Monster>();
+		ourVillains = new ArrayList<LivingThing>();
 		
 		Monster monster1 = new Monster("Steve", 80, "Ogre");
 		Monster monster2 = new Monster("Bob", 75, "Vampire");
 		Monster monster3 = new Monster("Joe", 90, "Warewolf");
-		Monster monster4 = new Monster("Billy", 100, "Psycho Killer");
+		Monster monster4 = new Monster("Billy", 100, "Psycho Killer Monster");
 		Monster monster5 = new Monster("Doug", 20, "Coding Monster");
 		
-		ourMonsters.add(monster1);
-		ourMonsters.add(monster2);
-		ourMonsters.add(monster3);
-		ourMonsters.add(monster4);
-		ourMonsters.add(monster5);
+		ourVillains.add(monster1);
+		ourVillains.add(monster2);
+		ourVillains.add(monster3);
+		ourVillains.add(monster4);
+		ourVillains.add(monster5);
 		
-		ourHero = new Hero("Clark Kent", 100, "Superman"); 
+		ourHero = new Hero("Clark Kent", 350, "Superman"); 
 	}
 	
 }
